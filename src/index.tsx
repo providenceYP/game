7ccd0606e47414c294276372d0ddc564bdef697a
DOXOPOKC <lilondin@gmail.com>
2021-08-19 22:00:53 +0700

    Добавил аватар

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import App from './layouts/App';
 import './styles/index.css';
 
