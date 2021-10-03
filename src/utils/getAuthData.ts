3efd31c541484ab1d2fb0e5c3cc6ad53bfd48fcc
Vadim Frolov <vadimf74@ya.ru>
2021-10-03 23:31:55 +0300

    utils

---
+++ src/utils/getAuthData.ts
@@ -0,0 +1,18 @@
+async function getAuthData() {
+  const headers = new Headers();
+  headers.append('accept', 'application/json');
+  const init = {
+    method: 'GET',
+    headers,
+  };
+  const response = await fetch(
+    'https://ya-praktikum.tech/api/v2/auth/user',
+    init,
+  );
+  const mediaType = response.headers.get('content-type');
+  if (mediaType.includes('json')) {
+    const authData = await response.json();
+    console.log(authData);
+  }
+}
+export default getAuthData;
---
+++ src/utils/getTokenInfo.ts
@@ -0,0 +1,28 @@
+async function getTokenInfo() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const queryCode = urlParams.get('code');
+  let tokenData;
+  const headers = new Headers();
+  headers.append('accept', 'application/json');
+  headers.append('Content-Type', 'application/json');
+  const body = JSON.stringify({
+    code: queryCode,
+    redirect_uri: 'http://localhost:3000',
+  });
+  const init = {
+    method: 'POST',
+    headers,
+    body,
+  };
+  const response = await fetch(
+    'https://ya-praktikum.tech/api/v2/oauth/yandex',
+    init,
+  );
+  const mediaType = response.headers.get('content-type');
+  if (mediaType.includes('json')) {
+    tokenData = await response.json();
+  }
+  return tokenData;
+}
+
+export default getTokenInfo;
