533c26f0aa86e0119abd581e00be087507da0ca7
Vadim Frolov <vadimf74@ya.ru>
2021-10-03 23:30:42 +0300

    det token data

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -5,10 +5,15 @@ import Layout from 'components/Layout';
 import Image from 'components/Image';
 import mainImage from 'images/main.jpeg';
 import getTokenInfo from 'utils/getTokenInfo';
+import getAuthData from 'utils/getAuthData';
 
 export default function Main() {
   useEffect(() => {
-    getTokenInfo();
+    async function oauthStart() {
+      await getTokenInfo();
+      await getAuthData();
+    }
+    oauthStart();
   }, []);
 
   return (
