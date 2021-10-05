6fcead8d8bc42aa11988be5949c3db6098e4acc8
Vadim Frolov <vadimf74@ya.ru>
2021-10-05 21:52:13 +0300

    credentials fixed

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -68,14 +68,15 @@ const Login: React.FC = (): JSX.Element => {
   );
 
   async function getServiceId() {
-    const init = {
-      method: 'GET',
-    };
+    // const init = {
+    //   method: 'GET',
+    // };
     const response = await fetch(
       'https://ya-praktikum.tech/api/v2/oauth/yandex/service-id/?redirect_uri=http://localhost:3000',
-      init,
+      { method: 'GET', credentials: 'include' },
     );
     const mediaType = response.headers.get('content-type');
+
     if (mediaType.includes('json')) {
       const serviceId = await response.json();
       window.location.href = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${serviceId.service_id}&redirect_uri=http://localhost:3000`;
--- src/utils/getAuthData.ts
+++ src/utils/getAuthData.ts
@@ -1,15 +1,17 @@
 async function getAuthData() {
   const headers = new Headers();
   headers.append('accept', 'application/json');
-  const init = {
+  // const init = {
+  //   method: 'GET',
+  //   headers,
+  // };
+  const response = await fetch('https://ya-praktikum.tech/api/v2/auth/user', {
     method: 'GET',
     headers,
-  };
-  const response = await fetch(
-    'https://ya-praktikum.tech/api/v2/auth/user',
-    init,
-  );
+    credentials: 'include',
+  });
   const mediaType = response.headers.get('content-type');
+
   if (mediaType.includes('json')) {
     const authData = await response.json();
     console.log(authData);
