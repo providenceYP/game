eed8b802175711324612c38f900878c250b17e37
Anastasia Gontareva <ana.gontareva@yandex.ru>
2021-10-27 12:04:00 +0300

    changed docker buuild

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY package.json /app/package.json
 RUN npm install
 COPY . /app
 RUN npm run build
-CMD ["npm", "start"]
+CMD ["npm", "start:build"]
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { app } = require('./dist/server.js');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log('Application is started on localhost:', port);
